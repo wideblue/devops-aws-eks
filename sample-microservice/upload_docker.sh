839bdd295c436c778d72cef42e19c641ec0f3f2f
Damjan <big.wide.blue@gmail.com>
2020-05-24 19:50:33 +0200

    feat: test microservice app

---
+++ sample-microservice/upload_docker.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="wideblue/udacity-microservice"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker tag udacity-microservice:latest $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
